3f5cfd5dce3808728bb222a41b2a8b6aebeac23a
AntonsWorkshop <167014478+AntonsWorkshop@users.noreply.github.com>
2024-04-15 14:24:48 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author "Anton's Workshop"
-description "https://github.com/"
+description "https://github.com/AntonsWorkshop/discord-radio"
 version '0.0.1'
 
 server_scripts {
